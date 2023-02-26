625667de2e8ab6c58e6e90791519c7f438cf3876
Salvatore <Skaldak1337@gmail.com>
2023-02-26 16:24:10 +0500

    update animation curve

--- lib/src/utils/constants.dart
+++ lib/src/utils/constants.dart
@@ -7,7 +7,7 @@ const Duration kMenuDuration = Duration(milliseconds: 300);
 
 /// Pull-down menu animation curve.
 @internal
-const Curve kCurve = Cubic(0.075, 0.82, 0.185, 1.065);
+const Curve kCurve = Cubic(0.075, 0.82, 0.4, 1.065);
 
 /// Pull-down menu reverse animation curve.
 @internal
